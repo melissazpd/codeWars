b6e0b653768044b473a91cf8ff3c33d26431c947
Melissa <zpdmelissa@gmail.com>
2022-04-12 20:33:45 -0500

    Are You Playing Banjo?

---
+++ JavaScript/8_kyu/Are You Playing Banjo?/solution.js
@@ -0,0 +1,7 @@
+function areYouPlayingBanjo(name) {
+    if (name.toLowerCase().charAt(0) == 'r') {
+        return name + " plays banjo";
+    } else {
+        return name + " does not play banjo";
+    }
+}
