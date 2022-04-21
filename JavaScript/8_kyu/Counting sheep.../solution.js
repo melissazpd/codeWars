d7bd4513e88e60cb94f76a28f9d771a274fce824
melissazpd <zpdmelissa@gmail.com>
2022-04-21 11:23:36 -0500

    Counting sheep...

---
+++ JavaScript/8_kyu/Counting sheep.../solution.js
@@ -0,0 +1,12 @@
+function countSheeps(arrayOfSheep) {
+    let numSheep = 0;
+
+    for (let i = 0; i <= arrayOfSheep.length; i++) {
+        if (arrayOfSheep[i] === true) {
+            numSheep++;
+        } else {
+            continue;
+        }
+    }
+    return numSheep;
+}
