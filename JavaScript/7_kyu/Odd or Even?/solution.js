93be77fae7f0a36588e850dcd958586605e7c0d2
Melissa <zpdmelissa@gmail.com>
2022-06-19 20:43:01 -0500

    Odd or Even?

---
+++ JavaScript/7_kyu/Odd or Even?/solution.js
@@ -0,0 +1,13 @@
+function oddOrEven(array) {
+    let sum = 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+
+    if (sum % 2 === 0) {
+        return 'even';
+    } else if (sum % 2 != 0) {
+        return 'odd';
+    }
+}
