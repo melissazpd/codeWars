9b88356c6b4adedc3cc8741f0ebc00cb4eb0dcfe
Melissa <zpdmelissa@gmail.com>
2022-06-19 22:15:10 -0500

    better calculate average solution

--- JavaScript/8_kyu/Calculate average/solution.js
+++ JavaScript/8_kyu/Calculate average/solution.js
@@ -7,3 +7,20 @@ function find_average(array) {
 
     return sum / array.length;
 }
+
+// Does not check if the array is empty.
+
+// Better solution below:
+
+function find_average(array) {
+    let sum = 0;
+
+    if (!array.length) {
+        return 0;
+    } else {
+        for (let i = 0; i < array.length; i++) {
+            sum += array[i];
+        }
+        return sum / array.length;
+    }
+}
